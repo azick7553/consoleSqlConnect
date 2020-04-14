9da7415e80ccbe3d712100689dbb55ea350e2976
azick7553 <azick7553@gmail.com>
2020-04-14 19:12:09 +0500

    Added SqlClient Package

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace consoleSqlConnect
 {
     class Program
     {
         static void Main(string[] args)
-        {
-           
+        {//Integrated Security=True
+           const string conString = @"Data source=172.16.137.139; Initial catalog=AlifAcademy; user id=sa; password=Root123.;
+           SqlConnection con = new SqlConnection();
         }
     }
 }
